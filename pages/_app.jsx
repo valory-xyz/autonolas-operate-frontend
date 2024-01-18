997fd3d6fe2c5e45779bd25007f0f858639d2576
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-01-18 18:23:05 +0000

    Add meta tags for social media sharing

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -14,14 +14,32 @@ import GlobalStyle from 'components/GlobalStyles';
 import { SITE } from 'util/constants';
 import initStore from '../store';
 
-const { TITLE } = SITE;
+const {
+  TITLE, DESCRIPTION, IMAGE_PATH, URL,
+} = SITE;
 
 const MyApp = ({ Component, pageProps }) => (
   <>
     <GlobalStyle />
     <Head>
-      <title>{TITLE}</title>
+      {/* <!-- Primary Meta Tags --> */}
+      <title>Operate | Olas</title>
       <meta name="title" content={TITLE} />
+      <meta name="description" content={DESCRIPTION} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={URL} />
+      <meta property="og:title" content={TITLE} />
+      <meta property="og:description" content={DESCRIPTION} />
+      <meta property="og:image" content={IMAGE_PATH} />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={URL} />
+      <meta property="twitter:title" content={TITLE} />
+      <meta property="twitter:description" content={DESCRIPTION} />
+      <meta property="twitter:image" content={IMAGE_PATH} />
     </Head>
     <ConfigProvider theme={THEME_CONFIG}>
       <WagmiConfigProvider config={wagmiConfig}>
--- util/constants.jsx
+++ util/constants.jsx
@@ -1,6 +1,8 @@
 export const SITE = {
   URL: 'https://operate.olas.network',
   TITLE: 'Olas Operate',
+  DESCRIPTION: 'Explore Olas agents, learn how to run and stake on them, potentially earn rewards.',
+  PATH: '/images/metatags-image.jpg',
 };
 
 export const OPERATOR_NAME = 'Olas (aka Autonolas) DAO';
